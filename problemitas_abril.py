920b6c7293a4bc57564ae29b2dd333fd6fa89a8d
lauzarg <lauzarate@mi.unc.edu.ar>
2025-04-09 21:38:58 -0300

    agregados los primeros problemas

---
+++ problemitas_abril.py
@@ -0,0 +1,36 @@
+""" 5) Hacer un programa que solicite un color por teclado e imprima “Puede pasar “ si el 
+color ingresado es verde , “Precaución “ si es amarillo , “No pasar “ si es rojo o “Color 
+inválido” si es cualquier otro."""
+
+""" color = str(input("ingrese un color escribiendo solo en minusculas: " ))
+
+def determinar_color(color):
+    if color == "verde":
+        return (f'Puede pasar')
+    elif color == "amarillo":
+        return (f'Precaución')
+    elif color == "rojo":
+        return (f'No pasar')
+    else:
+        return (f'Color inválido')
+
+
+print(determinar_color(color)) """
+
+
+"""7) Hacer un programa que cuente del 1 al 100 inclusive e imprima sólo los números pares """
+
+""" numeros_pares =  []
+
+def encontrar_pares(numeros):
+ 
+
+    return numeros_pares =+ numero
+
+print(encontrar_pares(numeros_pares))
+ """
+
+numeros_pares= []
+for i in range (101):
+        if i % 2 == 0:
+                print(i)
